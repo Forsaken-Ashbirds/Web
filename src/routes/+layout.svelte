20ff6e7f9707cd7ca21857077a951536df73f163
Magnus Leßmann (@MarkL4YG) <github@m-lessmann.de>
2023-07-31 17:45:02 +0200

    💬 Update footer text and TeamSpeak url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import Header from './Header.svelte';
 	import '../style/styles.scss';
 	import '../style/main.scss';
-
 </script>
 
 <div class="app">
@@ -13,13 +12,12 @@
 	</main>
 
 	<footer>
-		<p data-sveltekit-preload-data='off' >
-			Besuche <a href="https://ts.forsaken-ashbirds.de" target="_blank">forsaken-ashbirds</a> um unserem TeamSpeak Sever
+		<p>Besuche <a href="ts3server://forsaken-ashbirds" target="_blank" rel="noopener noreferrer"
+					  data-sveltekit-preload-data='off'>forsaken-ashbirds</a> um unserem TeamSpeak Sever
 			beizutreten.
 		</p>
-		<span
-			>© {new Date().getFullYear().toString()} Forsaken Ashbirds |
-			<a href="/impressum">Impressum</a></span
-		>
+		<p>© {new Date().getFullYear().toString()} Forsaken Ashbirds |
+			<a href="/impressum">Impressum</a>
+		</p>
 	</footer>
 </div>
