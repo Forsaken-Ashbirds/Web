2668950e7c6a257cdd74deeee59e03c0cd5bc9b5
Magnus Leßmann (@MarkL4YG) <github@m-lessmann.de>
2024-01-23 19:15:19 +0100

    :construction: Start work on fonts with headline styles

---
+++ src/routes/dev/+page.svelte
@@ -0,0 +1,164 @@
+<script>
+	import {dev} from "$app/environment";
+</script>
+
+{#if dev}
+	<div class="text-column">
+		<h1>Development page</h1>
+		<p>This page is used to test out different styles.</p>
+		<section id="headlines-and-text">
+			<h1>Headline - 1</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor malesuada mauris. Duis id
+				turpis ante. In eu nibh semper, sollicitudin ligula nec, dapibus ligula. Ut consequat hendrerit
+				ultrices. Phasellus feugiat congue auctor. Suspendisse dapibus imperdiet nibh a porta. Etiam mi est,
+				lacinia non laoreet eget, congue sed mi. Aenean vulputate sit amet magna vel pretium. Curabitur
+				efficitur pharetra pretium. Integer quis imperdiet ante. Nunc sem velit, tempus eget convallis eget,
+				mattis at metus. Sed venenatis quis sem volutpat vestibulum. Nam in turpis id mi maximus aliquam vitae
+				vitae justo. In mi magna, pellentesque vel arcu at, commodo tempor enim. Fusce in diam sollicitudin,
+				volutpat felis non, convallis turpis. Nunc eget sodales magna, non tristique ligula.</p>
+			<p>Ut ut felis condimentum, vehicula velit et, fermentum justo. Nunc blandit, nulla ut vulputate convallis,
+				purus leo vulputate metus, id pharetra justo erat sit amet urna. Morbi quis varius risus. Nullam aliquam
+				felis nec ligula tempor accumsan. Duis tincidunt bibendum dolor ac dictum. Duis ullamcorper dolor odio,
+				id dictum eros suscipit eu. Phasellus fermentum ullamcorper eros, nec pulvinar neque tristique sed. Nam
+				laoreet orci tempor, elementum enim molestie, lacinia sem. Quisque consectetur dignissim luctus. Duis
+				ullamcorper tincidunt accumsan. Sed sagittis porttitor magna, eu finibus elit placerat non. Orci varius
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.</p>
+			<p>Suspendisse potenti. Mauris sit amet laoreet velit, eu ullamcorper justo. Sed porta sollicitudin odio.
+				Praesent semper, nibh a semper volutpat, erat nisl posuere quam, vitae volutpat lectus leo quis justo.
+				Cras ut eleifend nisl. Donec ut ornare arcu. Phasellus fringilla egestas blandit. Nulla facilisi. Aenean
+				non sem elit. Donec sagittis tortor tempus felis efficitur, at bibendum nisi ornare. Morbi odio quam,
+				hendrerit vel tellus lobortis, semper bibendum dolor. Nulla feugiat arcu nec ipsum placerat, quis luctus
+				dolor placerat. Nunc et ante volutpat ante malesuada commodo sit amet fermentum nisl. Pellentesque
+				maximus vel nibh eget dignissim. Sed condimentum urna in hendrerit ornare. Curabitur nec sem ut nulla
+				venenatis lobortis et vitae diam.</p>
+			<h2>Headline - 2</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor malesuada mauris. Duis id
+				turpis ante. In eu nibh semper, sollicitudin ligula nec, dapibus ligula. Ut consequat hendrerit
+				ultrices. Phasellus feugiat congue auctor. Suspendisse dapibus imperdiet nibh a porta. Etiam mi est,
+				lacinia non laoreet eget, congue sed mi. Aenean vulputate sit amet magna vel pretium. Curabitur
+				efficitur pharetra pretium. Integer quis imperdiet ante. Nunc sem velit, tempus eget convallis eget,
+				mattis at metus. Sed venenatis quis sem volutpat vestibulum. Nam in turpis id mi maximus aliquam vitae
+				vitae justo. In mi magna, pellentesque vel arcu at, commodo tempor enim. Fusce in diam sollicitudin,
+				volutpat felis non, convallis turpis. Nunc eget sodales magna, non tristique ligula.</p>
+			<p>Ut ut felis condimentum, vehicula velit et, fermentum justo. Nunc blandit, nulla ut vulputate convallis,
+				purus leo vulputate metus, id pharetra justo erat sit amet urna. Morbi quis varius risus. Nullam aliquam
+				felis nec ligula tempor accumsan. Duis tincidunt bibendum dolor ac dictum. Duis ullamcorper dolor odio,
+				id dictum eros suscipit eu. Phasellus fermentum ullamcorper eros, nec pulvinar neque tristique sed. Nam
+				laoreet orci tempor, elementum enim molestie, lacinia sem. Quisque consectetur dignissim luctus. Duis
+				ullamcorper tincidunt accumsan. Sed sagittis porttitor magna, eu finibus elit placerat non. Orci varius
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.</p>
+			<p>Suspendisse potenti. Mauris sit amet laoreet velit, eu ullamcorper justo. Sed porta sollicitudin odio.
+				Praesent semper, nibh a semper volutpat, erat nisl posuere quam, vitae volutpat lectus leo quis justo.
+				Cras ut eleifend nisl. Donec ut ornare arcu. Phasellus fringilla egestas blandit. Nulla facilisi. Aenean
+				non sem elit. Donec sagittis tortor tempus felis efficitur, at bibendum nisi ornare. Morbi odio quam,
+				hendrerit vel tellus lobortis, semper bibendum dolor. Nulla feugiat arcu nec ipsum placerat, quis luctus
+				dolor placerat. Nunc et ante volutpat ante malesuada commodo sit amet fermentum nisl. Pellentesque
+				maximus vel nibh eget dignissim. Sed condimentum urna in hendrerit ornare. Curabitur nec sem ut nulla
+				venenatis lobortis et vitae diam.</p>
+			<h3>Headline - 3</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor malesuada mauris. Duis id
+				turpis ante. In eu nibh semper, sollicitudin ligula nec, dapibus ligula. Ut consequat hendrerit
+				ultrices. Phasellus feugiat congue auctor. Suspendisse dapibus imperdiet nibh a porta. Etiam mi est,
+				lacinia non laoreet eget, congue sed mi. Aenean vulputate sit amet magna vel pretium. Curabitur
+				efficitur pharetra pretium. Integer quis imperdiet ante. Nunc sem velit, tempus eget convallis eget,
+				mattis at metus. Sed venenatis quis sem volutpat vestibulum. Nam in turpis id mi maximus aliquam vitae
+				vitae justo. In mi magna, pellentesque vel arcu at, commodo tempor enim. Fusce in diam sollicitudin,
+				volutpat felis non, convallis turpis. Nunc eget sodales magna, non tristique ligula.</p>
+			<p>Ut ut felis condimentum, vehicula velit et, fermentum justo. Nunc blandit, nulla ut vulputate convallis,
+				purus leo vulputate metus, id pharetra justo erat sit amet urna. Morbi quis varius risus. Nullam aliquam
+				felis nec ligula tempor accumsan. Duis tincidunt bibendum dolor ac dictum. Duis ullamcorper dolor odio,
+				id dictum eros suscipit eu. Phasellus fermentum ullamcorper eros, nec pulvinar neque tristique sed. Nam
+				laoreet orci tempor, elementum enim molestie, lacinia sem. Quisque consectetur dignissim luctus. Duis
+				ullamcorper tincidunt accumsan. Sed sagittis porttitor magna, eu finibus elit placerat non. Orci varius
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.</p>
+			<p>Suspendisse potenti. Mauris sit amet laoreet velit, eu ullamcorper justo. Sed porta sollicitudin odio.
+				Praesent semper, nibh a semper volutpat, erat nisl posuere quam, vitae volutpat lectus leo quis justo.
+				Cras ut eleifend nisl. Donec ut ornare arcu. Phasellus fringilla egestas blandit. Nulla facilisi. Aenean
+				non sem elit. Donec sagittis tortor tempus felis efficitur, at bibendum nisi ornare. Morbi odio quam,
+				hendrerit vel tellus lobortis, semper bibendum dolor. Nulla feugiat arcu nec ipsum placerat, quis luctus
+				dolor placerat. Nunc et ante volutpat ante malesuada commodo sit amet fermentum nisl. Pellentesque
+				maximus vel nibh eget dignissim. Sed condimentum urna in hendrerit ornare. Curabitur nec sem ut nulla
+				venenatis lobortis et vitae diam.</p>
+			<h4>Headline - 4</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor malesuada mauris. Duis id
+				turpis ante. In eu nibh semper, sollicitudin ligula nec, dapibus ligula. Ut consequat hendrerit
+				ultrices. Phasellus feugiat congue auctor. Suspendisse dapibus imperdiet nibh a porta. Etiam mi est,
+				lacinia non laoreet eget, congue sed mi. Aenean vulputate sit amet magna vel pretium. Curabitur
+				efficitur pharetra pretium. Integer quis imperdiet ante. Nunc sem velit, tempus eget convallis eget,
+				mattis at metus. Sed venenatis quis sem volutpat vestibulum. Nam in turpis id mi maximus aliquam vitae
+				vitae justo. In mi magna, pellentesque vel arcu at, commodo tempor enim. Fusce in diam sollicitudin,
+				volutpat felis non, convallis turpis. Nunc eget sodales magna, non tristique ligula.</p>
+			<p>Ut ut felis condimentum, vehicula velit et, fermentum justo. Nunc blandit, nulla ut vulputate convallis,
+				purus leo vulputate metus, id pharetra justo erat sit amet urna. Morbi quis varius risus. Nullam aliquam
+				felis nec ligula tempor accumsan. Duis tincidunt bibendum dolor ac dictum. Duis ullamcorper dolor odio,
+				id dictum eros suscipit eu. Phasellus fermentum ullamcorper eros, nec pulvinar neque tristique sed. Nam
+				laoreet orci tempor, elementum enim molestie, lacinia sem. Quisque consectetur dignissim luctus. Duis
+				ullamcorper tincidunt accumsan. Sed sagittis porttitor magna, eu finibus elit placerat non. Orci varius
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.</p>
+			<p>Suspendisse potenti. Mauris sit amet laoreet velit, eu ullamcorper justo. Sed porta sollicitudin odio.
+				Praesent semper, nibh a semper volutpat, erat nisl posuere quam, vitae volutpat lectus leo quis justo.
+				Cras ut eleifend nisl. Donec ut ornare arcu. Phasellus fringilla egestas blandit. Nulla facilisi. Aenean
+				non sem elit. Donec sagittis tortor tempus felis efficitur, at bibendum nisi ornare. Morbi odio quam,
+				hendrerit vel tellus lobortis, semper bibendum dolor. Nulla feugiat arcu nec ipsum placerat, quis luctus
+				dolor placerat. Nunc et ante volutpat ante malesuada commodo sit amet fermentum nisl. Pellentesque
+				maximus vel nibh eget dignissim. Sed condimentum urna in hendrerit ornare. Curabitur nec sem ut nulla
+				venenatis lobortis et vitae diam.</p>
+			<h5>Headline - 5</h5>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor malesuada mauris. Duis id
+				turpis ante. In eu nibh semper, sollicitudin ligula nec, dapibus ligula. Ut consequat hendrerit
+				ultrices. Phasellus feugiat congue auctor. Suspendisse dapibus imperdiet nibh a porta. Etiam mi est,
+				lacinia non laoreet eget, congue sed mi. Aenean vulputate sit amet magna vel pretium. Curabitur
+				efficitur pharetra pretium. Integer quis imperdiet ante. Nunc sem velit, tempus eget convallis eget,
+				mattis at metus. Sed venenatis quis sem volutpat vestibulum. Nam in turpis id mi maximus aliquam vitae
+				vitae justo. In mi magna, pellentesque vel arcu at, commodo tempor enim. Fusce in diam sollicitudin,
+				volutpat felis non, convallis turpis. Nunc eget sodales magna, non tristique ligula.</p>
+			<p>Ut ut felis condimentum, vehicula velit et, fermentum justo. Nunc blandit, nulla ut vulputate convallis,
+				purus leo vulputate metus, id pharetra justo erat sit amet urna. Morbi quis varius risus. Nullam aliquam
+				felis nec ligula tempor accumsan. Duis tincidunt bibendum dolor ac dictum. Duis ullamcorper dolor odio,
+				id dictum eros suscipit eu. Phasellus fermentum ullamcorper eros, nec pulvinar neque tristique sed. Nam
+				laoreet orci tempor, elementum enim molestie, lacinia sem. Quisque consectetur dignissim luctus. Duis
+				ullamcorper tincidunt accumsan. Sed sagittis porttitor magna, eu finibus elit placerat non. Orci varius
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.</p>
+			<p>Suspendisse potenti. Mauris sit amet laoreet velit, eu ullamcorper justo. Sed porta sollicitudin odio.
+				Praesent semper, nibh a semper volutpat, erat nisl posuere quam, vitae volutpat lectus leo quis justo.
+				Cras ut eleifend nisl. Donec ut ornare arcu. Phasellus fringilla egestas blandit. Nulla facilisi. Aenean
+				non sem elit. Donec sagittis tortor tempus felis efficitur, at bibendum nisi ornare. Morbi odio quam,
+				hendrerit vel tellus lobortis, semper bibendum dolor. Nulla feugiat arcu nec ipsum placerat, quis luctus
+				dolor placerat. Nunc et ante volutpat ante malesuada commodo sit amet fermentum nisl. Pellentesque
+				maximus vel nibh eget dignissim. Sed condimentum urna in hendrerit ornare. Curabitur nec sem ut nulla
+				venenatis lobortis et vitae diam.</p>
+			<h6>Headline - 6</h6>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor malesuada mauris. Duis id
+				turpis ante. In eu nibh semper, sollicitudin ligula nec, dapibus ligula. Ut consequat hendrerit
+				ultrices. Phasellus feugiat congue auctor. Suspendisse dapibus imperdiet nibh a porta. Etiam mi est,
+				lacinia non laoreet eget, congue sed mi. Aenean vulputate sit amet magna vel pretium. Curabitur
+				efficitur pharetra pretium. Integer quis imperdiet ante. Nunc sem velit, tempus eget convallis eget,
+				mattis at metus. Sed venenatis quis sem volutpat vestibulum. Nam in turpis id mi maximus aliquam vitae
+				vitae justo. In mi magna, pellentesque vel arcu at, commodo tempor enim. Fusce in diam sollicitudin,
+				volutpat felis non, convallis turpis. Nunc eget sodales magna, non tristique ligula.</p>
+			<p>Ut ut felis condimentum, vehicula velit et, fermentum justo. Nunc blandit, nulla ut vulputate convallis,
+				purus leo vulputate metus, id pharetra justo erat sit amet urna. Morbi quis varius risus. Nullam aliquam
+				felis nec ligula tempor accumsan. Duis tincidunt bibendum dolor ac dictum. Duis ullamcorper dolor odio,
+				id dictum eros suscipit eu. Phasellus fermentum ullamcorper eros, nec pulvinar neque tristique sed. Nam
+				laoreet orci tempor, elementum enim molestie, lacinia sem. Quisque consectetur dignissim luctus. Duis
+				ullamcorper tincidunt accumsan. Sed sagittis porttitor magna, eu finibus elit placerat non. Orci varius
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.</p>
+			<p>Suspendisse potenti. Mauris sit amet laoreet velit, eu ullamcorper justo. Sed porta sollicitudin odio.
+				Praesent semper, nibh a semper volutpat, erat nisl posuere quam, vitae volutpat lectus leo quis justo.
+				Cras ut eleifend nisl. Donec ut ornare arcu. Phasellus fringilla egestas blandit. Nulla facilisi. Aenean
+				non sem elit. Donec sagittis tortor tempus felis efficitur, at bibendum nisi ornare. Morbi odio quam,
+				hendrerit vel tellus lobortis, semper bibendum dolor. Nulla feugiat arcu nec ipsum placerat, quis luctus
+				dolor placerat. Nunc et ante volutpat ante malesuada commodo sit amet fermentum nisl. Pellentesque
+				maximus vel nibh eget dignissim. Sed condimentum urna in hendrerit ornare. Curabitur nec sem ut nulla
+				venenatis lobortis et vitae diam.</p>
+		</section>
+	</div>
+{/if}
+
+{#if !dev}
+	<div class="text-column">
+		<h2>Nothing to see here</h2>
+		<p>This page is for development purposes only...</p>
+		<p>Here is a cookie for finding it though: 🍪</p>
+	</div>
+{/if}
