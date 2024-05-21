6d1b0f328c2e6e7d5bdfad23cbdcc6e83f388051
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2024-05-21 16:14:27 +0200

    lint and Time

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,9 +13,9 @@
 
 	<div class="aboutus">
 		<TalkTab direction={Direction.Left}>
-			Herzlich willkommen bei den Forsaken Ashbirds! Wir sind ein lebendiger Gaming-Clan, der sich
-			durch die gemeinsame Leidenschaft für aufregende Abenteuer und unvergessliche Spielerlebnisse
-			vereint.
+			Herzlich willkommen bei den Forsaken Ashbirds! Wir sind ein lebendiger Gaming-Clan, der
+			sich durch die gemeinsame Leidenschaft für aufregende Abenteuer und unvergessliche
+			Spielerlebnisse vereint.
 			<br />
 			<br />
 			Mit einer beeindruckenden Geschichte der Kameradschaft und Exzellenz ziehen wir durch verschiedene
@@ -28,25 +28,25 @@
 			<br />
 			<br />
 			Egal, ob wir fantastische Reiche erobern, in intensiven PvP-Kämpfen unsere Gegner herausfordern
-			oder uns in fesselnde Geschichten vertiefen, die Forsaken Ashbirds streben immer danach, uns zu
-			verbessern und den Sieg zu erringen.
+			oder uns in fesselnde Geschichten vertiefen, die Forsaken Ashbirds streben immer danach,
+			uns zu verbessern und den Sieg zu erringen.
 		</TalkTab>
 
 		<TalkTab direction={Direction.Left}>
 			Wir laden euch herzlich ein, sich uns anzuschließen und das Potenzial einer vereinten
-			Spielgemeinschaft zu entdecken, in der Freundschaften geschmiedet, Fähigkeiten verfeinert und
-			unvergessliche Erinnerungen geschaffen werden.<br />
-			<br /> Gemeinsam werden wir die Grenzen der virtuellen Welt überwinden und ein Vermächtnis schaffen,
-			das noch Generationen in Erinnerung behalten werden. Werde Teil der Forsaken Ashbirds und erlebe
-			Gaming auf einer neuen, entspannten Ebene.
+			Spielgemeinschaft zu entdecken, in der Freundschaften geschmiedet, Fähigkeiten
+			verfeinert und unvergessliche Erinnerungen geschaffen werden.<br />
+			<br /> Gemeinsam werden wir die Grenzen der virtuellen Welt überwinden und ein Vermächtnis
+			schaffen, das noch Generationen in Erinnerung behalten werden. Werde Teil der Forsaken Ashbirds
+			und erlebe Gaming auf einer neuen, entspannten Ebene.
 		</TalkTab>
 
 		<TalkTab direction={Direction.Right}>
 			Unsere Community ist für alle Spieler ab 18 Jahren offen. Wir sind ein deutsch/englisch
-			sprachiger Clan, aber auch Spieler aus anderen Ländern sind bei uns herzlich willkommen. Wir
-			sind ein Community-Clan, der sich auf das gemeinsame Spielen konzentriert.<br />
-			<br /> Wir sind keine professionelle E-Sport-Organisation und haben auch nicht vor, eine zu werden.
-			Wir sind ein Clan, der sich auf das gemeinsame Spielen konzentriert.
+			sprachiger Clan, aber auch Spieler aus anderen Ländern sind bei uns herzlich willkommen.
+			Wir sind ein Community-Clan, der sich auf das gemeinsame Spielen konzentriert.<br />
+			<br /> Wir sind keine professionelle E-Sport-Organisation und haben auch nicht vor, eine
+			zu werden. Wir sind ein Clan, der sich auf das gemeinsame Spielen konzentriert.
 		</TalkTab>
 	</div>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import staticDeployment from '@sveltejs/adapter-static';
-import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
-import {fileURLToPath} from 'node:url'
-import {readFileSync} from 'node:fs';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { fileURLToPath } from 'node:url';
+import { readFileSync } from 'node:fs';
 
 const file = fileURLToPath(new URL('package.json', import.meta.url));
 const json = readFileSync(file, 'utf8');
@@ -20,8 +20,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: staticDeployment(),
-	},
+		adapter: staticDeployment()
+	}
 };
 
 export default config;
